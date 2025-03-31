c31dd4a88856832e13fa4eed9388a9b5ce108890
puppy1012 <puppy1012@naver.com>
2025-03-31 23:26:17 +0900

    api 패턴 추가

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,6 +3,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -15,5 +16,27 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name")String name,Model model){
         model.addAttribute("name",name);
         return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody //json으로 반환이 기본이다
+    public String helloString(@RequestParam(value = "name")String name){
+        return "hello"+name;//hello spring
+    }
+    @GetMapping("hello-api") //api 버전
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello{
+        private String name;
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name=name;
+        }
+
     }
 }
